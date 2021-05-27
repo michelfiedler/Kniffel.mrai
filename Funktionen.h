8d3cbee09ff5f8a5805e989743ce0ff90b0caee2
Michel Fiedler <michel.fiedler@tu-braunschweig.de>
2021-05-27 23:34:22 +0200

    Basis Funktionen geschrieben
    Zufällige Würfelfunktion, Zählfunktion, sowie eine Summenfunktion

--- Funktionen.h
+++ Funktionen.h
@@ -1,4 +1,48 @@
 #ifndef FUNKTIONEN_H
 #define FUNKTIONEN_H
-
+#include <iostream>
 #endif // FUNKTIONEN_H
+
+int countN (int*, int, int);
+int sum (int*, int);
+void rolldice (int*, int*);
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+//Funktionsdefinitionen
+
+int countN (int* zeiger, int laenge, int N) //Zählfunktionen
+{
+    int anz=0;
+    for (int i=0; i<laenge; i++)
+    {
+        if (zeiger[i]==N) anz++;
+    }
+    return anz;
+}
+
+
+void rolldice (int* zeiger, int* behalten) //Zufällige Würfelfunktion
+{
+    for (int k=0; k<5; k++)
+    {
+        if (behalten[k]==0) {zeiger[k]=(rand()%6)+1;}
+    }
+}
+
+
+int sum (int* zeiger, int laenge) //Summenfunktion
+{
+    int summe=0;
+    for (int r=0; r<laenge; r++) summe+=zeiger[r];
+    return summe;
+}
+
+
+
+
+
+
+
