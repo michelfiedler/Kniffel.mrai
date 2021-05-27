82984a972d3e0c43bba81d1fd11692dc60d2f926
Michel Fiedler <michel.fiedler@tu-braunschweig.de>
2021-05-27 20:13:48 +0200

    testbranch

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main()
 {
     cout << "Hello World!" << endl;
     //so ich will nur mal testen ob ich das jetzt ändern kann und wie das so funktioniert
+
+    //hiermit will ich testen wie man mit mehreren branches arbeitet
     return 0;
 }
