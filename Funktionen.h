1f9e64add961b6f50465cf966d4ed495b634c44a
Michel Fiedler <michel.fiedler@tu-braunschweig.de>
2021-05-27 23:29:13 +0200

    Header Datei Funktionen erstellt

---
+++ Funktionen.h
@@ -0,0 +1,4 @@
+#ifndef FUNKTIONEN_H
+#define FUNKTIONEN_H
+
+#endif // FUNKTIONEN_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
+#include <ctime>
+#include <Funktionen.h>
 
 using namespace std;
 
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    //so ich will nur mal testen ob ich das jetzt ändern kann und wie das so funktioniert
 
-    //hiermit will ich testen wie man mit mehreren branches arbeitet
-
-    //Zeile 10 war die erste Änderung, jetzt kann ich hier noch mehr ändern, bevor ich die Zweige zusammenführe
-
-
-    //jetzt nehme ich noch diese kleine Änderung im Mastervor und schaue was passiert
-
-    // Inka macht auch ihren ersten Kommentar!
     return 0;
 }
