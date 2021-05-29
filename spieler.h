a7c18e22b90b11cb3a4ecbbf5f4eab53d032d274
r-djuren <r.djuren@tu-braunschweig.de>
2021-05-29 14:38:46 +0200

    Methoden für die Eingabe und Abfrage der Spielernamen erstellt

--- spieler.h
+++ spieler.h
@@ -15,6 +15,24 @@ class Spieler
     public:
         int Punktzahl;
         int* Spielstand;
+
+        void set_Name(char*);
+        char* get_Name();
 };
+
+//Methodendefinitionen
+
+void Spieler::set_Name(char* name_neu)
+{
+    for (int i=0; i<15; i++)
+    {
+       Name[i]=name_neu[i];
+    }
+}
+
+char* Spieler::get_Name()
+{
+    return (this->Name);
+}
 
 #endif // SPIELER_H
