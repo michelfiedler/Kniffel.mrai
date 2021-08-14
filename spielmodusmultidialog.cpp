c4cb5c75e7fc78b3b3c3c3f19410df221c207a16
r-djuren <r.djuren@tu-braunschweig.de>
2021-08-14 11:55:42 +0200

    Weitere Strukturierung des SpielmodusDialogs

--- spielmodusmultidialog.cpp
+++ spielmodusmultidialog.cpp
@@ -1,5 +1,11 @@
 #include "spielmodusmultidialog.h"
 #include "ui_spielmodusmultidialog.h"
+#include "data.h"
+#include "Funktionen.h"
+
+#include <QObject>
+#include <QLabel>
+#include <QMessageBox>
 
 spielmodusMultiDialog::spielmodusMultiDialog(QWidget *parent) :
     QDialog(parent),
