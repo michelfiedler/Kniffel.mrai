f4589d5c6832a546fb3d60d4d3c13af6438c8c58
r-djuren <r.djuren@tu-braunschweig.de>
2021-08-14 12:34:41 +0200

    Kniffelblatt an gewählte Spieleranzahl angepasst

--- spielmodusmultidialog.cpp
+++ spielmodusmultidialog.cpp
@@ -12,9 +12,32 @@ spielmodusMultiDialog::spielmodusMultiDialog(QWidget *parent) :
     ui(new Ui::spielmodusMultiDialog)
 {
     ui->setupUi(this);
+
+    ui->tW_SpielstandMulti->setColumnCount(data::spieleranzahl+2);  //Anzahl der Spalten im Kniffelblatt an Spieleranzahl anpassen
+    for(int i=0; i<=data::spieleranzahl+1; i++)     //i läuft von 0 bis Anzahl der Spieler -1 für den Index der Spalten
+    {
+        data::spieler[i].Spielstand = new int [13]; //Ein Spielstand Feld für die Spieler
+        data::spieler[i].reset_Spielstand();
+
+        ui->tW_SpielstandMulti->setHorizontalHeaderItem(i,new QTableWidgetItem(QString(data::spieler[i].mName)));   //Namen der Spieler im Kniffelblatt eintragen
+    }
+
 }
 
 spielmodusMultiDialog::~spielmodusMultiDialog()
 {
     delete ui;
+
+    //Dynamisch angeforderten Speicher wieder freigeben
+    for(int i=0; i<=data::spieleranzahl+1; i++)
+    {
+        delete [] data::spieler[i].Spielstand;
+        data::spieler[i].Spielstand = NULL;
+    }
 }
+
+void spielmodusMultiDialog::on_buttonBox_rejected()
+{
+    this->close();
+}
+
